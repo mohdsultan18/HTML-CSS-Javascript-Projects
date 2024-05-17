31d86ea215d89990030b575b65e23e6110124869
Mohd Sultan <132590302+mohdsultan18@users.noreply.github.com>
2024-05-17 16:37:48 +0530

    Add files via upload

---
+++ 66 weatherio/assets/js/api.js
@@ -0,0 +1,43 @@
+/**
+ * @license MIT
+ * @fileoverview All api related stuff like api_key, api request etc.
+ * @copyright mohdsultan18
+ * @author mohdsultan18
+ */
+
+'use strict';
+
+const api_key = "92298649ef54c1e134225ab2210cefe7";
+
+/**
+ * Fetch data from server
+ * @param {string} URL API url
+ * @param {Function} callback callback
+ */
+export const fetchData = function (URL, callback) {
+  fetch(`${URL}&appid=${api_key}`)
+    .then(res => res.json())
+    .then(data => callback(data));
+}
+
+export const url = {
+    currentWeather(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
+    },
+    forecast(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
+    },
+    airPollution(lat, lon) {
+        return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+    },
+    reverseGeo(lat, lon) {
+        return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+    },
+    /**
+     * @param {string} query Search query e.g.: "London", "New York"
+     */
+    geo(query) {
+        return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+    }
+}
+
